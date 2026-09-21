87a1e4b2b0445f757d89fac2dfad42ae11de0d0b
OshiSharma1222 <163647851+OshiSharma1222@users.noreply.github.com>
2026-09-21 19:11:14 +0530

    Add settings window

---
+++ src/Wallup/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,33 @@
+using System.Windows;
+using System.Windows.Input;
+using Wallup.ViewModels;
+
+namespace Wallup.Views;
+
+internal partial class SettingsWindow : Window
+{
+    private readonly TaskListViewModel _viewModel;
+
+    internal SettingsWindow(TaskListViewModel viewModel)
+    {
+        _viewModel = viewModel;
+        DataContext = viewModel;
+        InitializeComponent();
+    }
+
+    private void OnDrag(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+
+    private void OnDoneClick(object sender, RoutedEventArgs e) => Hide();
+
+    protected override void OnDeactivated(EventArgs e)
+    {
+        base.OnDeactivated(e);
+        _viewModel.SaveSettings();
+    }
+}
